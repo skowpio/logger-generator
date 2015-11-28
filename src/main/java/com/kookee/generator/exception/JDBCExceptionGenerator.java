08d9090bb90b10f7cc6864438a414eb54a53f748
100lec <lbombol@gmail.com>
2015-11-28 02:32:58 +0100

    asd

--- src/main/java/com/kookee/generator/exception/JDBCExceptionGenerator.java
+++ src/main/java/com/kookee/generator/exception/JDBCExceptionGenerator.java
@@ -8,9 +8,10 @@ import java.sql.SQLException;
  * Created by kuki on 2015-11-01.
  */
 public class JDBCExceptionGenerator implements ExceptionGenerator {
+	//komentarz
     public void generate() {
         try {
-            Connection con = DriverManager.getConnection("");
+            Connection con = DriverManager.getConnection("asd");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
